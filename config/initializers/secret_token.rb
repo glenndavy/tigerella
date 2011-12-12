94206a5cdc28a2e66ca0e644023caaa28f6015b4
Glenn Davy <glenn@davy.net.au>
2011-12-13 08:17:13 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tigerella::Application.config.secret_token = '1107a29723fc548298a35ba1ed72d9e34ec2e2b12aa7baf11d90054ebffac0f16ec791b417963ade6c08b4ab6cc60faac292bd50ee4f6e1dee1f04ade213aadd'
